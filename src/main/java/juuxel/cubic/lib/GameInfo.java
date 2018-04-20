75e6ba62db0d42a23d628b4088ed73c900d923d9
Juuxel <kasperi.kauppi@gmail.com>
2018-04-20 13:29:07 +0300

    Bump up the version number

--- src/main/java/juuxel/cubic/lib/GameInfo.java
+++ src/main/java/juuxel/cubic/lib/GameInfo.java
@@ -3,5 +3,5 @@ package juuxel.cubic.lib;
 public final class GameInfo
 {
     public static final String NAME = "Cubic";
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "0.4.1";
 }
